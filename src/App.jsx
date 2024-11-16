1f1f14168858c5b9c26101472b5a9b27e149c0af
sashuttle <sashuttle25@outlook.com>
2024-11-16 16:16:16 -0600

    added Navbar & styling to the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,55 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './styles/App.css';
+import './styles/Navigation.css'
+import About from'./components/About';
+import Education from './components/Education';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+
 
 function App() {
   return (
-    <div className="background">
-      <h1 className="sam">Samantha<br></br>Shuttleworth</h1>
-
-    </div>
+    <Router>
+      <div className="background">
+        <h1 className="sam">Samantha<br></br>Shuttleworth</h1>
+        <p className="dev">Full Stack Web Developer</p>
+        <div className="homeNav">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/about">About Me</Link>
+              </li>
+              <li>
+                <Link to="/education">Education</Link>
+              </li>
+              <li>
+                <Link to="/skills">Skills</Link>
+              </li>
+              <li>
+                <Link to="/projects">Projects</Link>
+              </li>
+              <li>
+                <Link to="/resume">Resume</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <Routes>
+          <Route path='/about' element={<About />} />
+          <Route path='/education' element={<Education />} />
+          <Route path='/skills' element={<Skills />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/resume' element={<Resume />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
